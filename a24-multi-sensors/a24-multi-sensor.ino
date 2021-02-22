57b2aac4d4776ab98f5f91eec80ab32d5be61d25
Jeremy Ellis <keyfreemusic@gmail.com>
2021-02-22 15:02:33 -0800

    Create a24-multi-sensor.ino

---
+++ a24-multi-sensors/a24-multi-sensor.ino
@@ -0,0 +1,13 @@
+      case GESTURE_UP:
+        Serial.println("Detected UP gesture");
+        break;
+
+      case GESTURE_DOWN:
+        Serial.println("Detected DOWN gesture");
+        break;
+
+      case GESTURE_LEFT:
+        Serial.println("Detected LEFT gesture");
+        break;
+
+      case GESTURE_RIGHT:
